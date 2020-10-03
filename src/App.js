797a5dacda20e90acaa0a2ce3e1706c5dda9faf9
eisenils <mail.eisenils@gmail.com>
2020-10-03 11:29:37 +0200

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDsmq1z-RKRaQNXEtJKnlzgPydeu2ujVkI",
+  authDomain: "superchat-a181a.firebaseapp.com",
+  databaseURL: "https://superchat-a181a.firebaseio.com",
+  projectId: "superchat-a181a",
+  storageBucket: "superchat-a181a.appspot.com",
+  messagingSenderId: "164910595920",
+  appId: "1:164910595920:web:1fb9515c64f0283fc07655",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
